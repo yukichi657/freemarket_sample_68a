4f40dc0d8d1f33c8a1d438cad25f4f4c81e0c413
oha66rada15 <n68.oharada.keita@gmail.com>
2020-02-12 15:19:30 +0900

    pemkey_change

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/shop.pem'] 
+                  keys: ['~/.ssh/free68a.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
